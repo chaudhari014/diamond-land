3e06cea6a19d4857eb4c7766bb7927127d6792cd
Pranavi-Kayapati <pranavikayapati01@gmail.com>
2023-02-25 22:34:23 +0530

    menu bar changed

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,9 @@
 let rings = document.querySelector(".menu2");
 let earrings = document.querySelector(".menu3");
 let bangles = document.querySelector(".menu4");
+let solitaires = document.querySelector(".menu5");
+let mangalsutras = document.querySelector(".menu6");
+let otherjewerellery = document.querySelector(".menu9");
 // console.log(hover.innerText);
 let ringsdiv = document.querySelector(".main-hover");
 let earringsdiv = document.querySelector(".main-hover2");
@@ -60,4 +63,59 @@ banglesdiv.addEventListener("mouseover", () => {
 
 banglesdiv.addEventListener("mouseout", () => {
   banglesdiv.style = "display:none;visibility:hidden";
+});
+
+//for solitaires
+solitaires.addEventListener("mouseover", () => {
+  ringsdiv.style = "display:block;visibility:visible";
+});
+
+solitaires.addEventListener("mouseout", () => {
+  ringsdiv.style = "display:none;visibility:hidden";
+});
+
+ringsdiv.addEventListener("mouseover", () => {
+  ringsdiv.style = "display:block;visibility:visible";
+});
+
+ringsdiv.addEventListener("mouseout", () => {
+  ringsdiv.style = "display:none;visibility:hidden";
+});
+
+//for mangalsutras
+
+mangalsutras.addEventListener("mouseover", () => {
+  banglesdiv.style = "display:block;visibility:visible";
+});
+
+mangalsutras.addEventListener("mouseleave", () => {
+  // console.log(div.innerHTML);
+  banglesdiv.style = "display:none;visibility:hidden";
+});
+
+banglesdiv.addEventListener("mouseover", () => {
+  banglesdiv.style = "display:block;visibility:visible";
+});
+
+banglesdiv.addEventListener("mouseout", () => {
+  banglesdiv.style = "display:none;visibility:hidden";
+});
+
+//for other jewellerys
+
+otherjewerellery.addEventListener("mouseover", () => {
+  earringsdiv.style = "display:block;visibility:visible";
+});
+
+otherjewerellery.addEventListener("mouseleave", () => {
+  // console.log(div.innerHTML);
+  earringsdiv.style = "display:none;visibility:hidden";
+});
+
+earringsdiv.addEventListener("mouseover", () => {
+  earringsdiv.style = "display:block;visibility:visible";
+});
+
+earringsdiv.addEventListener("mouseout", () => {
+  earringsdiv.style = "display:none;visibility:hidden";
 });
